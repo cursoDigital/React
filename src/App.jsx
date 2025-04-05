da96f6f393b117008630f4c604a3a8ce87b28909
Eduardo Aragão de Sousa <EduardoSousa0722@digitalcollege.internal>
2025-04-05 10:26:42 -0300

    lado esquerdo feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import LoginImage from "./assets/login.svg"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Olá, mundo</h1>
+    <main className="w-full h-screen flex">
+      {/* Esquerda */}
+      <div className="bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 w-1/2 h-full flex flex-col justify-center items-center">
+        <img src={LoginImage} alt="Imagem ilustrativa de login" className=" w-[450px]" />
+        <h1>Secure Login Portal.</h1>
+        <p>Acess your dashboard securely with our protected login system. Your data is encrypted and secure.</p>
+      </div>
+      {/* Direita */}
+      <div className="bg-blue-300 w-1/2 h-full"></div>
+    </main>    
   )
 }
 
